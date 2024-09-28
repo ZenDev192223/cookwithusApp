572c838164b5b01b79d5a8a9c8f13335774c02fc
Suhan376 <zendev192223@gmail.com>
2024-09-28 10:16:13 +0530

    first commit

---
+++ src/screens/HomeScreen.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function HomeScreen() {
+  return (
+    <View className="flex-1 items-center justify-center text-8xl font-bold">
+      <Text>
+        Home Screen
+      </Text>
+    </View>
+  );
+}
+
+export default HomeScreen;
---
+++ src/screens/WelcomeScreen.js
@@ -0,0 +1,11 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+import "../App.css"
+
+export default function WelcomeScreen() {
+  return (
+    <View className="flex-1 justify-center items-center space-y-10 bg-amber-500">
+      <Text>WelcomeScreen</Text>
+    </View>
+  )
+}
